0878df27f1a7b7aa675b0e5e7dd4b096d5a0e40f
Joshua Whitley <josh.whitley@autoware.org>
2020-10-03 11:30:57 -0500

    Initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+// Copyright 2020 Whitley Software Services, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/// \copyright Copyright 2020 Whitley Software Services, LLC
+/// All rights reserved.
+/// \file
+/// \brief This file is the main entrypoint for the ffree command-line application
+
+#include <cxxopts.hpp>
+
+int32_t main(int32_t argv, char ** argc)
+{
+  return 0;
+}
