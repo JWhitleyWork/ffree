114895cc3e96a9e6c5f0f49a36bede06f9a0aae6
Joshua Whitley <josh.whitley@autoware.org>
2020-10-03 15:17:54 -0500

    Ading some basic command-line arguments.

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,63 @@
 /// \file
 /// \brief This file is the main entrypoint for the ffree command-line application
 
-#include <cxxopts.hpp>
+#include <iostream>
+#include <memory>
+#include <string>
 
-int32_t main(int32_t argv, char ** argc)
+#include <cxxopts.hpp>
+#include <logging.hpp>
+
+using namespace ffree::logging;
+
+int32_t main(int32_t argc, char ** argv)
 {
+  cxxopts::Options opts("ffree", "The Files and Folders Rules Evaluation Engine");
+  opts.add_options()
+    ("h,help", "Print help and exit")
+    ("l,log-level", "Logging level", cxxopts::value<std::string>()->default_value("warn"))
+    ("r,rules", "Rules XML file", cxxopts::value<std::string>());
+
+  bool print_help = false;
+  LogLevel logging_level;
+
+  try {
+    const auto result = opts.parse(argc, argv);
+
+    // Handle arguments
+    std::string log_level_string = (result.count("l") > 0) ?
+      result["l"].as<std::string>() : result["log-level"].as<std::string>();
+
+    if (log_level_string == "debug") {
+      logging_level = LogLevel::DEBUG;
+    } else if (log_level_string == "info") {
+      logging_level = LogLevel::INFO;
+    } else if (log_level_string == "warn") {
+      // This is the default log level;
+      logging_level = LogLevel::WARN;
+    } else if (log_level_string == "error") {
+      logging_level = LogLevel::ERROR;
+    } else if (log_level_string == "none") {
+      logging_level = LogLevel::NONE;
+    } else {
+      std::cout << "\nValid logging levels are none, error, warn (default), info, and debug.\n";
+      print_help = true;
+    }
+
+    if (result.count("help") > 0 || result.count("h") > 0) {
+      print_help = true;
+    }
+  } catch (cxxopts::OptionParseException ex) {
+    std::cerr << "\nError parsing options: " << ex.what() << "\n\n";
+    std::cout << opts.help() << std::endl;
+    return -1;
+  }
+
+  if (print_help) {
+    std::cout << "\n";
+    std::cout << opts.help() << std::endl;
+    return 0;
+  }
+
   return 0;
 }
